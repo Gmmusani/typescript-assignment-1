85ffcf051f7c5406784b05fc7bda39ca7a870311
ghulam <gulammustafa2930@gmail.com>
2024-03-02 00:14:58 +0500

    my first commit

---
+++ a1.js
@@ -0,0 +1,15 @@
+//string
+var a = "hellow";
+console.log(a);
+//number
+var b = 123;
+console.log(b);
+//boolean
+var c = true;
+console.log(c);
+//null
+var d = null;
+console.log(d);
+//undefined
+var e;
+console.log(e);
---
+++ a1.ts
@@ -0,0 +1,19 @@
+//string
+let a: string = "hellow";
+console.log(a);
+
+//number
+let b: number = 123
+console.log(b);
+
+//boolean
+let c: boolean = true
+console.log(c);
+
+//null
+let d: null = null
+console.log(d);
+
+//undefined
+let e: undefined
+console.log(e);
